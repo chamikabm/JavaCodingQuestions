8c12892927f390afb1760a06066bb9727b5a51b1
chamikab <kasun.chamika@gmail.com>
2018-09-23 22:50:35 +0530

    Reverse a Numver algo 2 added.

--- src/NumberManipulation/ReverseANumber.java
+++ src/NumberManipulation/ReverseANumber.java
@@ -3,13 +3,30 @@ package NumberManipulation;
 public class ReverseANumber {
 
     public static void main(String[] args) {
-        int numberToReverse = 123;
-        System.out.println(reverseNumber(numberToReverse));
+        int numberToReverse = 4568;
+//        System.out.println(reverseNumber(numberToReverse));
+        System.out.println(reverseNumber2(numberToReverse));
     }
 
     private static int reverseNumber(int numberToReverse) {
+        String number = String.valueOf(numberToReverse);
         int reversedNumber = 1;
 
-        return 0;
+        for (int i = number.length() -1; i >0; i--) {
+            reversedNumber += Integer.parseInt(String.valueOf(number.charAt(i))) * Math.pow(10, i);
+        }
+
+        return reversedNumber;
+    }
+
+    private static int reverseNumber2(int numberToReverse) {
+        int reversedNumber = 0;
+        while (numberToReverse != 0 ) {
+            reversedNumber = reversedNumber*10 + numberToReverse % 10;
+            numberToReverse = numberToReverse / 10;
+        }
+
+
+        return reversedNumber;
     }
 }
