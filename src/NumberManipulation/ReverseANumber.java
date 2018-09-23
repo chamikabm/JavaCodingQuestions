2b8ce6f3fbd1701612e2da0ea18737e0e3f8caeb
chamikab <kasun.chamika@gmail.com>
2018-09-23 22:29:34 +0530

    Reverse a Numver algo added.

---
+++ src/NumberManipulation/ReverseANumber.java
@@ -0,0 +1,15 @@
+package NumberManipulation;
+
+public class ReverseANumber {
+
+    public static void main(String[] args) {
+        int numberToReverse = 123;
+        System.out.println(reverseNumber(numberToReverse));
+    }
+
+    private static int reverseNumber(int numberToReverse) {
+        int reversedNumber = 1;
+
+        return 0;
+    }
+}
