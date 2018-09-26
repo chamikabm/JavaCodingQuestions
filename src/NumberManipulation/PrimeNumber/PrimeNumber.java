f9f8ce8cdff3ab9e99e46efd27173666c4e645e6
chamikab <kasun.chamika@gmail.com>
2018-09-26 23:17:22 +0530

    Prime number checking algo 2 added.

--- src/NumberManipulation/PrimeNumber/PrimeNumber.java
+++ src/NumberManipulation/PrimeNumber/PrimeNumber.java
@@ -1,36 +1,47 @@
 package NumberManipulation.PrimeNumber;
 
 /**
- * A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
- * A natural number greater than 1 that is not a prime number is called a composite number. For example, 5 is prime,
- * as only 1 and 5 divide it, whereas 6 is composite, since it has the divisors 2 and 3 in addition to 1 and 6.
- * The fundamental theorem of arithmetic establishes the central role of primes in number theory: any integer greater
- * than 1 can be expressed as a product of primes that is unique up to ordering.
- * This theorem requires excluding 1 as a prime.
+ * Prime Number Definition:
+ * <p>
+ * A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller
+ * natural numbers. A natural number greater than 1 that is not prime is called a composite number. For example,
+ * 5 is prime, as only 1 and 5 divide it, whereas 6 is composite, since it has the divisors 2 and 3 in
+ * addition to 1 and 6.
  */
 public class PrimeNumber {
     public static void main(String[] args) {
-        int numberToCheck = 1;
+        int numberToCheck = 4;
         System.out.println(isPrimeNumber(numberToCheck));
+        System.out.println(isPrimeNumber2(numberToCheck));
     }
 
     private static boolean isPrimeNumber(int numberToCheck) {
-        if (numberToCheck < 1) {
+        if (numberToCheck <= 2) {
             return false;
         }
 
-        if (numberToCheck == 1) {
-            return true;
-        }
-
         int divider = numberToCheck;
-
         while (--divider != 1) {
-            if (numberToCheck%divider == 0) {
+            if (numberToCheck % divider == 0) {
                 return false;
             }
         }
 
         return true;
     }
+
+    private static boolean isPrimeNumber2(int numberToCheck) {
+        if (numberToCheck <= 2) {
+            return false;
+        }
+
+        int sqrt = (int) Math.sqrt(numberToCheck) + 1;
+        for (int i = 2; i < sqrt; i++) {
+            if (numberToCheck % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
