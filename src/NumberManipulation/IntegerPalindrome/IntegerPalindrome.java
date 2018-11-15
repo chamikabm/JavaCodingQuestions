8fe640e95d27bfb59662bf6160c4eed9b735e86b
chamikab <kasun.chamika@gmail.com>
2018-11-15 22:33:47 +0530

    Added Number Palindrom check algorithm.

---
+++ src/NumberManipulation/IntegerPalindrome/IntegerPalindrome.java
@@ -0,0 +1,34 @@
+package NumberManipulation.IntegerPalindrome;
+
+/**
+ * An integer is called palindrome if its equal to its reverse
+ *
+ * e.g. 1001 is a palindrome but 1234 is not because the reverse of 1234 is 4321 which is not equal to 1234.
+ *
+ */
+public class IntegerPalindrome {
+
+    public static void main(String[] args) {
+        int number = 1001;
+        System.out.println(isPalindromeNumber(number));
+
+        int number2 = 1234;
+        System.out.println(isPalindromeNumber(number2));
+    }
+
+    private static boolean isPalindromeNumber(int number) {
+        if (Integer.toString(number).length()  < 2) {
+            return true;
+        }
+
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            reverse = reverse*10 + palindrome%10;
+            palindrome = palindrome/10;
+        }
+
+        return number == reverse;
+    }
+}
