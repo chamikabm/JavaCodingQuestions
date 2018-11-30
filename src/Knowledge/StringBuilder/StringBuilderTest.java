e0fd26fd9959636f0a8ed5b2fbbbcd29fa691c87
chamikab <kasun.chamika@gmail.com>
2018-11-30 20:32:12 +0530

    Revert sout changes.:

--- src/Knowledge/StringBuilder/StringBuilderTest.java
+++ src/Knowledge/StringBuilder/StringBuilderTest.java
@@ -7,7 +7,7 @@ public class StringBuilderTest {
         StringBuilder b = a.append("de"); //  StringBuilder changes its own state and returns a reference to itself!
         b = b.append("f").append("g");
 
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
     }
 }
