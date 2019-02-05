63ab2220230f7a04c52a8748c78bfc01f0d7c464
chamikab <kasun.chamika@gmail.com>
2019-02-05 20:38:20 +0530

    Added algo .

---
+++ src/OtherCommonQuestions/SmallestSubStringOfAllChars/SmallestSubStringOfAllChars.java
@@ -0,0 +1,15 @@
+package OtherCommonQuestions.SmallestSubStringOfAllChars;
+
+public class SmallestSubStringOfAllChars {
+
+    public static void main(String[] args) {
+        String str = "xzyxyxxxssdwdf";
+        char[] chrs = new char[]{'x', 'y', 'z'};
+        System.out.println(getSmallerCharArray(chrs, str));
+    }
+
+    private static String getSmallerCharArray(char[] chrs, String str) {
+        //
+        return null;
+    }
+}
